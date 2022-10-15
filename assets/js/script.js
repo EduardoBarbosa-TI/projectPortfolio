fdb8c3f6c5622af8bab8cf82bca47897aa7df66b
Eduardo Barbosa Bernardes <eduardobb@rede.ulbra.br>
2022-10-15 15:28:43 -0300

    create elements new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,42 @@
-const redirectionPages  = document.querySelectorAll(".page");
+const redirectionPages = document.querySelectorAll(".page");
 
 redirectionPages.forEach(page => {
+
     page.addEventListener("click", () => {
-        if(page.classList.contains("active")){
+
+        if (page.classList.contains("active")) {
             return;
         }
 
-        const btnPageSelected = document.querySelector(".page.active");
+        selectedPages(page);
 
-        
-      
-        btnPageSelected.classList.remove("active");
+        removePage();
 
-        page.classList.add("active");
-
-        const contentSelected = document.querySelector(".information.active");
-
-        contentSelected.classList.remove("active");
-
+        renderPageSelected(page);
     })
 })
+
+function selectedPages(page) {
+    const btnPageSelected = document.querySelector(".page.active");
+
+    btnPageSelected.classList.remove("active");
+
+    page.classList.add("active");
+
+    return
+
+}
+
+function removePage() {
+    const contentSelected = document.querySelector(".information.active");
+
+    contentSelected.classList.remove("active");
+}
+
+function renderPageSelected(page) {
+    const idElementPageInformation = `page-${page.id}`
+
+    const informationSelected = document.getElementById(idElementPageInformation)
+
+    informationSelected.classList.add("active")
+}
