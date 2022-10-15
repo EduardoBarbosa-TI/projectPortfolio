80b6a7b775a75d504b68a9539e12dc1282878bb7
eduardobarbosa-ti <eduardobb@rede.ulbra.br>
2022-10-14 22:45:01 -0300

    Create file in JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const redirectionPages  = document.querySelectorAll(".page");
+
+redirectionPages.forEach(page => {
+    page.addEventListener("click", () => {
+        if(page.classList.contains("active")){
+            return;
+        }
+
+        const btnPageSelected = document.querySelector(".page.active");
+
+        
+      
+        btnPageSelected.classList.remove("active");
+
+        page.classList.add("active");
+
+        const contentSelected = document.querySelector(".information.active");
+
+        contentSelected.classList.remove("active");
+
+        const idPageSelected = `page${}`
+
+    })
+})
